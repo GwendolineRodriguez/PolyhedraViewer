d1290ddc4dac4d399119714abcf8cd93711af13e
Gwendoline Rodriguez <gwendoline.rodriguez@globalx.com.au>
2021-11-04 09:37:03 +1000

    first commit

---
+++ src/app.module.js
@@ -0,0 +1,112 @@
+import * as THREE from "three";
+import Polyhedron from "./polyhedron.module";
+
+const polyhedronSize = 16;
+const getAccentColor = () =>
+  getComputedStyle(document.body).getPropertyValue("--accent-color");
+const accentColor = getAccentColor();
+const theme = {
+  light: window.matchMedia("(prefers-color-scheme: light)"),
+  dark: window.matchMedia("(prefers-color-scheme: dark)"),
+  accentColor: getAccentColor(),
+};
+
+const _darkCSS = document.querySelectorAll(
+  "link[rel=stylesheet][media*=prefers-color-scheme][media*=dark]"
+);
+const _lightCSS = document.querySelectorAll(
+  "link[rel=stylesheet][media*=prefers-color-scheme][media*=light]"
+);
+
+const canva = document.getElementById("canva");
+const themeToggler = document.getElementById("themeToggler");
+const initToggler = () => {
+  if (window.matchMedia("(prefers-color-scheme: dark)")) {
+    themeToggler.checked = true;
+  }
+};
+initToggler();
+const tetrahedronBtn = document.getElementById("tetrahedron");
+const cubeBtn = document.getElementById("cube");
+const octahedronBtn = document.getElementById("octahedron");
+const dodecahedronBtn = document.getElementById("dodecahedron");
+const icosaHedronBtn = document.getElementById("icosahedron");
+
+const tetrahedronGeometry = new THREE.TetrahedronGeometry(polyhedronSize, 0);
+const cubeGeometry = new THREE.BoxGeometry(
+  polyhedronSize,
+  polyhedronSize,
+  polyhedronSize
+);
+const octahedronGeometry = new THREE.OctahedronGeometry(polyhedronSize, 0);
+const dodecahedronGeometry = new THREE.DodecahedronGeometry(polyhedronSize, 0);
+const icosahedronGeometry = new THREE.IcosahedronGeometry(polyhedronSize, 0);
+
+const tetrahedron = new Polyhedron(tetrahedronGeometry, accentColor);
+const cube = new Polyhedron(cubeGeometry, accentColor);
+const octahedron = new Polyhedron(octahedronGeometry, accentColor);
+const dodecahedron = new Polyhedron(dodecahedronGeometry, accentColor);
+const icosahedron = new Polyhedron(icosahedronGeometry, accentColor);
+
+canva.appendChild(icosahedron.renderer.domElement);
+
+updateCanva = (e) => {
+  const polyhedron = e.currentTarget.polyhedron;
+  canva.childNodes[0].replaceWith(polyhedron.renderer.domElement);
+};
+
+updateTheme = (e) => {
+  switchTheme().then(() => updatePolyhedronTheme());
+};
+
+switchTheme = () => {
+  return new Promise((resolve) => {
+    if (theme.light) {
+      theme.light = false;
+      theme.dark = true;
+      _darkCSS.forEach((link) => {
+        link.media = "not all";
+        link.disabled = true;
+      });
+      _lightCSS.forEach((link) => {
+        link.media = "all";
+        link.disabled = false;
+        onload = resolve();
+      });
+    } else {
+      theme.light = true;
+      theme.dark = false;
+      _lightCSS.forEach((link) => {
+        link.media = "not all";
+        link.disabled = true;
+      });
+      _darkCSS.forEach((link) => {
+        link.media = "all";
+        link.disabled = false;
+        onload = resolve();
+      });
+    }
+  });
+};
+
+const updatePolyhedronTheme = () => {
+  theme.accentColor = getAccentColor();
+  tetrahedron.updateTheme(theme);
+  cube.updateTheme(theme);
+  octahedron.updateTheme(theme);
+  dodecahedron.updateTheme(theme);
+  icosahedron.updateTheme(theme);
+};
+
+tetrahedronBtn.polyhedron = tetrahedron;
+cubeBtn.polyhedron = cube;
+octahedronBtn.polyhedron = octahedron;
+icosaHedronBtn.polyhedron = icosahedron;
+dodecahedronBtn.polyhedron = dodecahedron;
+
+tetrahedronBtn.addEventListener("click", updateCanva);
+cubeBtn.addEventListener("click", updateCanva);
+octahedronBtn.addEventListener("click", updateCanva);
+icosaHedronBtn.addEventListener("click", updateCanva);
+dodecahedronBtn.addEventListener("click", updateCanva);
+themeToggler.addEventListener("click", updateTheme);
---
+++ src/polyhedron.module.js
@@ -0,0 +1,87 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { LineMaterial } from "three/examples/jsm/lines/LineMaterial";
+import { LineSegmentsGeometry } from "three/examples/jsm/lines/LineSegmentsGeometry";
+import { Wireframe } from "three/examples/jsm/lines/Wireframe";
+import { WireframeGeometry2 } from "three/examples/jsm/lines/WireframeGeometry2";
+
+class Polyhedron {
+  constructor(polyhedron, accentColor) {
+    this.geometry = polyhedron;
+    this.accentColor = parseInt(accentColor.replace("#", "0x"));
+    this.width = window.innerWidth;
+    this.height = window.innerHeight * 0.7;
+    this.viewAngle = 35;
+    this.nearClippingPane = 1;
+    this.farClippingPane = 1000;
+    this.init();
+  }
+
+  init = () => {
+    this.camera = new THREE.PerspectiveCamera(
+      this.viewAngle,
+      this.width / this.height,
+      this.nearClippingPane,
+      this.farClippingPane
+    );
+    this.camera.position.set(-50, 0, 50);
+    this.scene = new THREE.Scene();
+
+    var edgesPavement = new THREE.EdgesGeometry(this.geometry);
+    var lineGeometry = new LineSegmentsGeometry().setPositions(
+      edgesPavement.attributes.position.array
+    );
+    this.matLine = new LineMaterial({
+      color: this.accentColor,
+      linewidth: 3,
+    });
+    this.mesh = new Wireframe(lineGeometry, this.matLine);
+    this.mesh.computeLineDistances();
+    this.mesh.scale.set(1, 1, 1);
+
+    this.scene.add(this.mesh);
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.width, this.height);
+    this.renderer.setAnimationLoop(this.animation);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableZoom = false;
+    window.addEventListener("resize", this.onWindowResize);
+    this.onWindowResize();
+  };
+
+  render = () => {
+    this.renderer.setClearColor(0x000000, 0);
+    this.renderer.setViewport(0, 0, this.width, this.height);
+    this.matLine.resolution.set(this.width, this.height);
+    this.renderer.render(this.scene, this.camera);
+  };
+
+  animation = (time) => {
+    this.mesh.rotation.y = time / 10000;
+    this.render();
+  };
+
+  stopAnimation = () => {
+    this.mesh.rotation.y = 0;
+    this.mesh.rotation.needsUpdate = true;
+    this.render();
+  };
+
+  onWindowResize = () => {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight * 0.7;
+
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
+
+    this.renderer.setSize(this.width, this.height);
+  };
+
+  updateTheme = (theme) => {
+    this.mesh.material.color.setHex(theme.accentColor.replace("#", "0x"));
+  };
+}
+
+export default Polyhedron;
